1b92907a2b680aea7a9527a386885348a9b79f0d
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-08-29 00:42:14 -0600

    Add files via upload

---
+++ IntroduccionPython.py
@@ -0,0 +1,37 @@
+# Apllicar Comentarios 
+
+#Imprimir mensajes
+print ('Hola Mundo') 
+
+#Tipo de Datos
+print (type ('Esto es un texto'))
+print ( type (4))
+print ( type (1.1))
+
+#Variables
+a = 4
+b = 6
+c = a + b
+
+print(c)
+
+#Concatenar
+a = 16 
+mi_texto = 'Yahir'
+print (a,mi_texto) 
+
+#Arreglos 
+
+Arreglo = ['A','B','C','D','E']
+#Imprimir Elemento del Arreglo
+print (Arreglo[3]) 
+
+#Matriz
+
+Matriz = [
+    ['A','B','C','D','E'],
+    ['F','G','H','I','J'],
+    ['K','L','M','N','0']
+]
+#Imprimir Elemento de La Matriz
+print ( Matriz[2][3])
