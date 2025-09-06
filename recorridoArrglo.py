24277d93f0dc3922f2dfd631028272a9eb187763
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-05 23:44:42 -0600

    Recorrer Arreglo

---
+++ recorridoArrglo.py
@@ -0,0 +1,16 @@
+
+
+Array = [2,3,4,5,6] #Declarar Arrglo 
+
+#Imprimir Arreglo 
+
+for Element in Array :
+    
+    print (Element)
+
+#Con rangos 
+
+for elementm,item in enumerate(Array):
+    
+    print (item)  #Imprimir Contenido de Posicion
+    print(elementm)  #Imprimir Posicion
