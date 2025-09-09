6cda4e4d6333ffd5fa0d1e0623c46603ee3a476d
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-08 22:26:08 -0600

    Recorrer Arreglos por Filas

---
+++ recorrerarreglofilas.py
@@ -0,0 +1,11 @@
+# Declarar la matriz
+Arreglo = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Recorrer la matriz con dos bucles for
+for i in range(3):
+    for j in range(3):
+        print(Arreglo[i][j], end=" ")  # end=" " evita el salto de línea automático
